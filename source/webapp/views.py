904f688463b26ea18f67c9dacfbd1c5cda67ea00
DinaraKA <d.karybekova@gmail.com>
2019-09-22 01:34:28 +0600

    добавила главную страницу, модивифицировала базовый шаблон и соответствующие записи в urls.py, views.py, переместила папку фикстур в webapp.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from webapp.models import Product
+
+
+def index_view(request):
+    products = Product.objects.filter(amount__gt=0).order_by('name','category')
+    return render(request, 'index.html', context={
+        'products': products
+    })
+
 
 
